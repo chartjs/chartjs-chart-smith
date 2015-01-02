a6a91e2b7fff9755e55c665b67767cc3922c7358
Evert Timberg <evert.timberg@gmail.com>
2015-01-02 16:32:15 -0500

    Scale labels now exist. Some labels move when popups occur for some reason

--- src/Chart.smith.js
+++ src/Chart.smith.js
@@ -67,10 +67,28 @@
 			// Smith charts use a unique scale. This needs to be written from scratch.
 			this.ScaleClass = Chart.Element.extend({
 				initialize: function(){
-					this.size = helpers.min([this.height, this.width]);
+					var xMargin = 0;
+					var yMargin = 0;
+					
+					if (this.showLabels)
+					{
+						// Labels mean move the drawingArea to the right
+						this.ctx.fillStyle = this.textColor;
+						this.ctx.font = this.font;
+						xMargin = this.ctx.measureText("0.0").width + 5;
+						
+						var dpi = 96;
+						if (window.devicePixelRatio) {
+							dpi *= window.devicePixelRatio;
+						}
+						
+						yMargin = this.fontSize * dpi / 72; // font size is in pts which on cavas equals px
+					}
+					
+					this.size = helpers.min([this.height - (2 * yMargin), this.width - (2 * xMargin)]);
 					this.drawingArea = (this.display) ? (this.size / 2) - (this.fontSize / 2 + this.backdropPaddingY) : (this.size / 2);
-					this.drawCenterX = (this.display) ? this.backdropPaddingX + (this.drawingArea / 2) : this.drawingArea / 2;
-					this.drawCenterY = (this.display) ? this.backdropPaddingY + (this.drawingArea / 2) : this.drawingArea / 2;
+					this.drawCenterX = (this.display) ? this.backdropPaddingX + xMargin + (this.drawingArea / 2) : xMargin + (this.drawingArea / 2);
+					this.drawCenterY = (this.display) ? this.backdropPaddingY + yMargin + (this.drawingArea / 2) : yMargin + (this.drawingArea / 2);
 				},
 				// Method to get the x,y position of a point given it's real and imaginary value
 				getPointPosition : function(real, imag) {
@@ -100,7 +118,10 @@
 					var ctx = this.ctx;
 					ctx.strokeStyle = this.lineColor;
 					ctx.lineWidth = this.lineWidth;
-			
+					
+					this.rLabels = [];
+					this.xLabels = [];
+					
 					if (this.display)
 					{
 						// How do we draw the circles? From http://care.iitd.ac.in/People/Faculty/bspanwar/crl713/smith_chart_basics.pdf
@@ -121,6 +142,17 @@
 							ctx.arc(x, this.drawCenterY, radius, 0, 2 * Math.PI);
 							ctx.closePath();
 							ctx.stroke();
+							
+							if (this.showLabels) {
+								// We draw labels at the left side of the circle but do not label 0
+								if (Math.round(r * 10) % 2 === 0) {
+									this.rLabels.push({
+										text : r,
+										x : x - radius,
+										y : this.drawCenterY
+									});
+								}
+							}
 						}, this);
 						
 						// Now we need to draw the impedance circles.
@@ -135,8 +167,8 @@
 						
 						// 0 Special case
 						ctx.beginPath();
-						ctx.moveTo(0, this.drawCenterY);
-						ctx.lineTo(this.drawingArea, this.drawCenterY);
+						ctx.moveTo(this.drawCenterX - (this.drawingArea / 2), this.drawCenterY);
+						ctx.lineTo(this.drawCenterX + (this.drawingArea / 2), this.drawCenterY);
 						ctx.stroke();
 						ctx.closePath();
 						
@@ -147,19 +179,19 @@
 							var yCoord = this.drawCenterY - xRadius;
 							
 							// Ok, these circles are a pain. They need to only be drawn in the region that intersects the resistance == 0 circle. This circle has a radius of 0.5 * this.drawingArea and is
-							// centered at (0.5 * this.drawingArea, 0.5 * this.drawingArea)
+							// centered at (drawCenterX, drawCenterY)
 							
-							// We will solve the intersection in polar coordinates and define (0, 0) as the center of the xCircle, ie (xCoord, yCoord)
+							// We will solve the intersection in polar coordinates and define the center of our coordinate system as the center of the xCircle, ie (xCoord, yCoord)
 							var r0 = Math.sqrt(Math.pow(xCoord - this.drawCenterX, 2) + Math.pow(yCoord - this.drawCenterY, 2));
-							var angle = Math.atan2(this.drawCenterY - yCoord, this.drawCenterX - xCoord);
+							var phi0 = Math.atan2(this.drawCenterY - yCoord, this.drawCenterX - xCoord);
 							
-							// A circle at location r0, angle with radius a is defined in polar coordinates by the equation
-							// r = r0 * cos(phi - angle) + sqrt(a^2 - ((r0^2) * sin^2(phi - angle)))
+							// A circle with center location r0,phi0 with radius a is defined in polar coordinates by the equation
+							// r = r0 * cos(phi - phi0) + sqrt(a^2 - ((r0^2) * sin^2(phi - phi0)))
 							// Our xCircle is defined by r = xRadius because of where we defined the 0,0 point
 							// Solving the intersection of these equations yields
-							// phi = 0.5 * arccos((xRad^2 - a^2) / (r0^2)) + angle
+							// phi = 0.5 * arccos((xRadius^2 - a^2) / (r0^2)) + phi0
 							var arccos = Math.acos((Math.pow(xRadius, 2) - Math.pow(this.drawingArea / 2, 2)) / Math.pow(r0, 2));
-							var phi2 = (0.5 * arccos) + angle;
+							var phi2 = (0.5 * arccos) + phi0;
 							
 							ctx.beginPath();
 							// These lines are always above the horizontal and always begin at 90 degrees
@@ -167,17 +199,89 @@
 							ctx.stroke();
 							ctx.closePath();
 							
+							if (this.showLabels) {
+								this.xLabels.push({
+									x : xCoord + (Math.cos(phi2) * xRadius),
+									y : yCoord + (Math.sin(phi2) * xRadius),
+									text : x
+								});
+							}
+							
 							// Negative circle
 							yCoord = this.drawCenterY + xRadius;
 							r0 = Math.sqrt(Math.pow(xCoord - this.drawCenterX, 2) + Math.pow(yCoord - this.drawCenterY, 2));
-							angle = Math.atan2(this.drawCenterY - yCoord, this.drawCenterX - xCoord); // atan2 should always return an angle in [-PI/2, 0) for these circles
+							phi0 = Math.atan2(this.drawCenterY - yCoord, this.drawCenterX - xCoord); // atan2 should always return an angle in [-PI/2, 0) for these circles
 							arccos = Math.acos((Math.pow(xRadius, 2) - Math.pow(this.drawingArea / 2, 2)) / Math.pow(r0, 2));
-							phi2 = (-0.5 * arccos) + angle;
+							phi2 = (-0.5 * arccos) + phi0;
 								
 							ctx.beginPath();
 							ctx.arc(xCoord, yCoord, xRadius, -1 / 2 * Math.PI, phi2, true);
 							ctx.stroke();
 							ctx.closePath();
+							
+							if (this.showLabels) {
+								this.xLabels.push({
+									x : xCoord + (Math.cos(phi2) * xRadius),
+									y : yCoord + (Math.sin(phi2) * xRadius),
+									text : x
+								});
+							}
+						}, this);
+						
+						ctx.fillStyle = this.textColor;
+						ctx.font = this.font;
+						
+						// Rotate canvas so that text is draw in correct orientation
+						
+						var prevX = 0;
+						var prevY = 0;
+						helpers.each(this.rLabels, function(label, labelIndex) {
+							ctx.save();
+							ctx.translate(label.x, label.y);
+							ctx.rotate(-0.5 * Math.PI);
+							ctx.fillText(label.text, 0, 0);
+							prevX = label.x;
+							prevY = label.y;
+							ctx.restore();
+						}, this);
+						
+						helpers.each(this.xLabels, function(label, labelIndex) {
+							var metrics = ctx.measureText(label.text);
+							var xDiff = 0;
+							
+							if (label.x < this.drawCenterX) {
+								xDiff = -1.0 * metrics.width;
+							}
+							
+							if (label.y > this.drawCenterY) {
+								ctx.textBaseline = 'top';
+							}
+							else if (label.y < this.drawCenterY) {
+								ctx.textBaseline = 'bottom';
+							}
+							else {
+								ctx.textBaseline = 'middle';
+							}
+							
+							var ang = Math.atan2(label.y - this.drawCenterY, label.x - this.drawCenterX);
+							
+							if ((ang < (-0.5 * Math.PI)) || (ang > (0.5 * Math.PI))) {
+								ang += Math.PI;
+							}
+							
+							/*if (ang < 0 && ang > (0.5 * Math.PI)) {
+								ang += (0.5 * Math.PI);
+							}
+							else if (ang > 0 && ang < (0.5 * Math.PI))
+							{
+								ang -= (0.5 * Math.PI);
+							}*/
+							
+							ctx.save();
+							ctx.translate(label.x, label.y);
+							ctx.rotate(ang);
+							ctx.fillText(label.text, xDiff, 0);
+							ctx.restore();
 						}, this);
 					}
 				},
