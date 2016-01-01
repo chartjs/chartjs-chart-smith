1ef4ad9d8b19fe64a90aa1cff0a6071ae386a01e
Evert Timberg <evert.timberg@gmail.com>
2016-01-01 14:17:04 -0500

    More gulpfile and package improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var concat = require('gulp-concat'),
 	gulp = require('gulp'),
-	replace = require('gulp-concat'),
+	jshint = require('gulp-jshint'),
+	package = require('./package.json'),
+	replace = require('gulp-replace'),
 	umd = require('gulp-umd'),
 	util = require('gulp-util');
 
@@ -8,12 +10,15 @@ var srcDir = './src/';
 var srcFiles = srcDir + '**.js';
 var buildDir = '.';
 
+gulp.task('build', buildTask);
+gulp.task('jshint', jsHintTask);
+
 function buildTask() {
 	return gulp.src(srcFiles)
 		.pipe(concat('Chart.Smith.js'))
 		.pipe(replace('{{ version }}', package.version))
 		.pipe(umd({
-			template: 'amdCommonWeb',
+			templateName: 'amdCommonWeb',
 			dependencies: function() {
 				return ['Chart']
 			}
@@ -21,4 +26,8 @@ function buildTask() {
 		.pipe(gulp.dest(buildDir));
 }
 
-gulp.task('build', buildTask);
+function jsHintTask() {
+	return gulp.src(srcFiles)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+}
