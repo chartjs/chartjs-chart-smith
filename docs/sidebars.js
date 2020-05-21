72b4e7769436568ba8224962e4c2267506f7f12d
Evert Timberg <evert.timberg@gmail.com>
2020-05-21 18:06:30 -0400

    Begin move to docusaurus

---
+++ docs/sidebars.js
@@ -0,0 +1,31 @@
+module.exports = {
+  someSidebar: {
+    Introduction: ['index'],
+    Configuration: [
+      'Configuration',
+    ],
+    // Developers: [
+    //  'developers/index',
+    //  'developers/api',
+    //  {
+    //    type: 'link',
+    //    label: 'TypeDoc',
+    //    href: 'typedoc/index.html'
+    //  },
+    //  'developers/updates',
+    //  'developers/plugins',
+    //  'developers/charts',
+    //  'developers/axes',
+    //  'developers/contributing'
+    // ],
+    // 'Additional Notes':[
+    //   'notes/comparison',
+    //    {
+    //      type: 'link',
+    //      label: 'Extensions',
+    //      href: 'https://github.com/chartjs/awesome'
+    //    },
+    //   'notes/license'
+    // ]
+  },
+};
